72e2456f52b18b90d52c0f8a545f616b7a6187be
sudo-ToR <55355210+sudo-ToR@users.noreply.github.com>
2019-12-17 15:44:33 +0100

    Add files via upload

--- converter_utils.c
+++ converter_utils.c
@@ -6,7 +6,7 @@
 /*   By: lnoirot <lnoirot@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/12/07 16:40:14 by lnoirot           #+#    #+#             */
-/*   Updated: 2019/12/09 19:28:39 by lnoirot          ###   ########.fr       */
+/*   Updated: 2019/12/17 14:48:13 by lnoirot          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,8 +20,7 @@ int		flag_0(char *str, int prec, int minus, va_list ap)
 
 	ret = 0;
 	length = 0;
-	if (*str == '.' || *str == '0')
-		prec = ft_atoi(str++);
+	prec += ((*str == '.' || *str == '0') && prec != 0) ? 0 : ft_atoi(++str);
 	length =  (*str == '*') ? prec - va_arg(ap, int) : prec - ft_atoi(str);
 	arg = va_arg(ap, int);
 	if (prec == 0 && arg != 0)
@@ -35,9 +34,8 @@ int		flag_0(char *str, int prec, int minus, va_list ap)
 	prec *= (prec > 0) ? 1 : -1;
 	while (prec-- > 0)
 		ret += write(1, "0", 1);
-	if (arg != 0)
-		ret += ft_putnbr_base_d(arg, "0123456789");
+	ret +=  (arg !=0) ? ft_putnbr_base_d(arg, "0123456789") : 0;
 	while (minus != 0 && length-- > 0)
 		ret+= write(1, " ", 1);
 	return (ret);
-}
+}			
